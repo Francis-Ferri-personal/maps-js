67cde8c8e57bab07ae804b15d6db2bf43d45b7c1
Francis Ferri <francisferrir@gmail.com>
2021-06-04 16:09:29 -0500

    Maps gas stations

---
+++ js/UI.js
@@ -0,0 +1,85 @@
+class UI {
+    constructor() {
+        // Instanciar la API
+        this.api = new API();
+
+        // Crear llos markers con layer group
+        this.markers = new L.LayerGroup();
+         // Iniciar el mapa
+         this.mapa = this.inicializarMapa();
+    }
+
+    inicializarMapa() {
+         // Inicializar y obtener la propiedad del mapa
+         const map = L.map('mapa').setView([19.390519, -99.3739778], 6);
+         const enlaceMapa = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+         L.tileLayer(
+             'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+             attribution: '&copy; ' + enlaceMapa + ' Contributors',
+             maxZoom: 18,
+             }).addTo(map);
+         return map;
+
+    }
+    mostrar_establecimientos(){
+        this.api.obtener_datos()
+            .then( datos => {
+                const resultado = datos.respuesta_json.results;
+                
+                // Ejecutar la funcion para mostar los pines
+                this.mostrar_pines(resultado);
+
+            });
+    }
+
+    mostrar_pines(datos){
+        // Limpiar los markers
+        this.markers.clearLayers();
+
+        // recorrer establecimienos
+        datos.forEach(dato => {
+            // Aplicamos un destructuring para extraer la informacion
+            
+            //<<<
+            const {latitude, longitude, calle, regular, premium} = dato;
+            //>>>
+
+            // Crear Popup
+            const opcones_popup = L.popup()
+                .setContent(`
+                <p>Calle: ${calle}</p>
+                <p><b>Regular:</b> ${regular}</p>
+                <p><b>Premium:</b> ${premium}</p>
+                `);
+
+            // agregar el PIN
+            const marker = new L.marker([
+                parseFloat(latitude),
+                parseFloat(longitude)
+            ]).bindPopup(opcones_popup);
+            this.markers.addLayer(marker);
+        });
+        this.markers.addTo(this.mapa);
+    }
+
+    // Buscador
+    obtener_sugerencias(busqueda){
+        this.api.obtener_datos()
+            .then(datos => {
+                //Obtener los datos
+                const resultado = datos.respuesta_json.results;
+                // Enviar el JSON y la busqueda para el filtrado
+                this.filtrar_sugerencias(resultado, busqueda);
+            });
+    }
+
+    // filtar las sugerencas en base al input
+    filtrar_sugerencias(resultados, busqueda){
+        // Filtrar con .filter
+        const filtro = resultados.filter(elemento => elemento.calle.indexOf(busqueda) !== -1);
+        console.log(filtro);
+
+        // mostrar los pines
+        this.mostrar_pines(filtro);
+    }
+}
---
+++ js/app.js
@@ -0,0 +1,22 @@
+// https://leafletjs.com/download.html
+const ui = new UI();
+
+document.addEventListener("DOMContentLoaded", () => {
+   ui.mostrar_establecimientos();  
+});
+
+// Habilitar busqueda de establecimientos
+const buscador =document.querySelector("#buscar input");
+
+
+//<<<
+buscador.addEventListener("input", () => {
+   if(buscador.value.length > 5){
+        // Buscar en la API
+        ui.obtener_sugerencias(buscador.value);
+   } else {
+    ui.mostrar_establecimientos();
+}
+});
+
+//>>>
